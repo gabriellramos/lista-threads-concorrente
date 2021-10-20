31fa3e613ff729351c698940b7db1e869672fcf4
gabriellramos <gabrielramosnascimento195@gmail.com>
2021-10-20 14:37:58 -0400

    Update q1.c

--- q1.c
+++ q1.c
@@ -1,35 +1,68 @@
+//descrição
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
-#define N 10
+#include <math.h>
 
-void *imprimePrimos(int inicio, int fim);
+#define N 99999
+#define NUM_THREADS getNumThreads()
+
+pthread_mutex_t mutexsum;
+int inicio=0, fim=999;
+int k;	
+
+int getNumThreads(){
+	return (N / 1000)+1;
+}
+
+void *imprimePrimos(void *tid){
+	int cont=0,i,aux,j;
+	int id = (int)tid;
+	
+
+	pthread_mutex_lock (&mutexsum);
+	printf ("\nTHREAD %d - (%d a %d)\n ",id, inicio, fim);
+	for (i=inicio;i<fim;i++){
+		if (i>1){
+			cont=1;
+			for (j=2;cont && j<=sqrt(i);j++){
+				if (i % j == 0){
+					cont = 0;
+					break;
+				}
+			}
+		}
+		
+		if (cont){
+			printf ("%5d\t",i);
+			k++;
+			if (k==10){
+				printf ("\n");
+				k=0;
+			}
+		}
+	}
+		
+	pthread_mutex_unlock (&mutexsum);
+	pthread_exit(NULL);
+}
 
 int main(){
-	pthread_t threads[N];
-	int i=0, status, inicio=0, fim=999;
+	pthread_mutex_init(&mutexsum, NULL);
+	printf ("%d",NUM_THREADS);
+	pthread_t threads[NUM_THREADS];
 	
-	for (i=0;i<N;i++){
-		printf ("THREAD %d - (%d a %d)\n ",i+1, inicio, fim);
-		pthread_create(&(threads[i]), NULL, imprimePrimos(inicio,fim), NULL);
-		inicio=fim+1;
-		fim+=999;
-		printf ("\n");
-	}
-	
-	for(i=0; i<N; i++) {
-		pthread_join(threads[i], NULL);
-	}
-		return 0;
+	int i, status;
+	for (i=0;i<NUM_THREADS;i++){
+		status = pthread_create(&(threads[i]), NULL, imprimePrimos, (void*)i);
+		_sleep(1);
+		inicio = fim+1;
+		fim += 1000;
 	}
 	
-	void *imprimePrimos(int inicio, int fim){
-	int i, cont=0, j,aux;
-	for (i=inicio;i<=fim;i++){
-		cont=1;
-		
-		for (j=3;cont && (j<=i/2);j+=2)
-			if (i % j == 0) cont =0;
-		
-		if (cont)
-			printf ("%d ", i);
+	for (i=0;i<NUM_THREADS;i++){
+		pthread_join(threads[i],NULL);
 	}
+	pthread_mutex_destroy(&mutexsum);
+	return 0;
 }
